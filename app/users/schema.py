8838445e0788ffbec77f5e45f117b4a70e05cb82
Charles Yusuf <yusufcharles@gmail.com>
2020-08-20 19:05:59 +0200

    feat: update and delete tracks

--- app/users/schema.py
+++ app/users/schema.py
@@ -1,11 +1,27 @@
 import graphene
 from graphene_django import DjangoObjectType
 from django.contrib.auth import get_user_model
+import graphql_jwt
 
 
 class UserType(DjangoObjectType):
     class Meta:
         model = get_user_model()
+
+
+class Query(graphene.ObjectType):
+    users = graphene.Field(UserType, id=graphene.Int(required=True))
+    current_user = graphene.Field(UserType)
+
+    def resolve_users(self, info, id):
+        return get_user_model().objects.get(id=id)
+
+    def resolve_current_user(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("No logged in!")
+        else:
+            return user
 
 
 class CreateUser(graphene.Mutation):
@@ -27,3 +43,7 @@ class CreateUser(graphene.Mutation):
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
